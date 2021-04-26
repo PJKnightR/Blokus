f12b72a6cef4d89baa6b356e82bd78b289d02228
PJKnightR <coolpj99@comcast.net>
2021-04-25 21:12:40 -0400

    Began Method stubs

---
+++ BlokusAI.java
@@ -0,0 +1,19 @@
+public class BlokusAI {
+
+    public void AlphaBetaSearch() {
+        //returns a piece placement
+    }
+
+    public void maxValue(int alpha, int beta) {
+
+    }
+
+    public void minValue(int alpha, int beta) {
+
+    }
+
+    public void getPlayableSpaces() {
+        //for each playable area for a piece, given each piece remaining,
+        // how many spots will be playable after placement
+    }
+}
